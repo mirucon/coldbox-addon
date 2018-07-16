4a8a68f7030ba34f5c9c5e25f5923c5993b163b3
Toshihiro Kanai <i@miruc.co>
2018-07-16 14:30:58 +0900

    Add option to deregister jQuery

--- inc/general.php
+++ inc/general.php
@@ -13,3 +13,13 @@ function coldbox_addon_social_links_attr() {
 	echo 'target="_blank"';
 }
 add_action( 'cd_social_links_attr', 'coldbox_addon_social_links_attr' );
+
+/**
+ * Deregister jQuery if the option is checked.
+ */
+function coldbox_addon_deregister_jquery() {
+	if ( cd_do_not_load_jquery() ) {
+			wp_deregister_script( 'jquery' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'cd_scripts' );
