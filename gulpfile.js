5ebd47ce2b1648ea51504a6a247de8ba198c66c9
Toshihiro Kanai <i@miruc.co>
2019-12-21 11:43:26 +0900

    chore(deps): Upgrade Composer deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,49 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var stylus = require('gulp-stylus');
-var autoprefixer = require('gulp-autoprefixer');
-var cleanCSS = require('gulp-clean-css');
+var gulp = require("gulp");
+var browserSync = require("browser-sync");
+var stylus = require("gulp-stylus");
+var autoprefixer = require("gulp-autoprefixer");
+var cleanCSS = require("gulp-clean-css");
 
-gulp.task( 'browser-sync', function () {
-  browserSync({
-    open: 'external',
-    notify: false,
-    proxy: "http://coldbox.vccw/"
-  });
+gulp.task("browser-sync", function() {
+	browserSync({
+		open: "external",
+		notify: false,
+		proxy: "http://coldbox.vccw/"
+	});
 });
-gulp.task( 'bs-reload', function () {
+gulp.task("bs-reload", function() {
 	browserSync.reload();
 });
-gulp.task( 'default', ['browser-sync'], function () {
-	gulp.watch("*.php", ['bs-reload']);
+gulp.task("default", ["browser-sync"], function() {
+	gulp.watch("*.php", ["bs-reload"]);
 });
 
-gulp.task( 'copy', function() {
-  return gulp.src(
-    [ '*.php', 'inc/*.php', 'readme.txt' ],
-    { base: '.' }
-  )
-  .pipe( gulp.dest( 'dist' ) );
-} );
+gulp.task("copy", function() {
+	return gulp
+		.src(["*.php", "inc/*.php", "readme.txt"], { base: "." })
+		.pipe(gulp.dest("dist"));
+});
 
-gulp.task( 'styl', ['stylus'], function () {
-	return gulp.src('inc/amp-style.css')
+gulp.task("styl", ["stylus"], function() {
+	return gulp
+		.src("inc/amp-style.css")
 		.pipe(cleanCSS())
-		.pipe(gulp.dest('inc/'))
-})
+		.pipe(gulp.dest("inc/"));
+});
 
-gulp.task( 'stylus', function () {
-	return gulp.src('inc/amp-style.styl')
+gulp.task("stylus", function() {
+	return gulp
+		.src("inc/amp-style.styl")
 		.pipe(stylus())
-		.pipe(autoprefixer({
-			browsers: ['> 1%', 'not ie 11'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('inc/'))
-})
+		.pipe(
+			autoprefixer({
+				browsers: ["> 1%", "not ie 11"],
+				cascade: false
+			})
+		)
+		.pipe(gulp.dest("inc/"));
+});
 
-gulp.task( 'watch', ['styl'], function () {
-	gulp.watch( 'inc/amp-style.styl', ['styl'])
-})
+gulp.task("watch", ["styl"], function() {
+	gulp.watch("inc/amp-style.styl", ["styl"]);
+});
